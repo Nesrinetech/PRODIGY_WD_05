a5f2bdca1c0650e2d0078c7db315a92e4f2f20d1
Nesrine <kabachinesrine34@gmail.com>
2024-09-22 18:46:45 +0100

    work

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 async function getWeather() {
     const city = document.getElementById('city').value;
-    const response = await fetch('http://localhost:5000/get-weather', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ city: city })
-    });
-
+    const API_KEY = '5cbedd07cdbdc3722fbe18c0c2651245';  
+    const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+      
+    const url = `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`; // 'metric' for Celsius
+   
+   
+ try {
+    const response = await fetch(url);
     const data = await response.json();
+  
     if (response.ok) {
         document.getElementById('weather').innerText = `Weather: ${data.weather}`;
         document.getElementById('temperature').innerText = `Temperature: ${data.temperature} Celsius`;
     } else {
         document.getElementById('weather').innerText = `Error: ${data.error}`;
     }
+} catch (error) {
+    console.error('Error fwtching the weather data:', error);
+    document.getElementById('weather').innerText =  'Error: Could not retrieve data';
+}
 }
